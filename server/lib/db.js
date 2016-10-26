45cb4080296b829f0f671e75d1ad434658b3150b
Nat York <natyork@gmail.com>
2016-10-26 18:46:26 +0000

    Fix mongo connection string issue

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,7 +1,8 @@
 "use strict";
+require('dotenv').config();
 
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let collection;
 
